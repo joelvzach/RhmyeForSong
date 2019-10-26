a116a11295322f133cde68191354a73caba323ab
Joel V Zachariah <joelvzach@gmail.com>
2019-10-26 23:27:06 +0530

    Update rap_helper.py

--- rap_helper.py
+++ rap_helper.py
@@ -1,7 +1,7 @@
 # Program to help aspiring rappers/poets develop their vocabulory
     
 # PART 1: Basic rhyming words predictor                     (DONE)
-# PART 2: Rhyming scheme basis - rhyming word predictor     (NOPE)
+# PART 2: Rhyming scheme basis - rhyming word predictor     (DONE)
 # PART 3: Syllable basis recommender                        (NOPE)
 # PART 4: GUI Input and Output system                       (NOPE)
 # PART 5: Audio Input System                                (NOPE)
@@ -11,31 +11,51 @@
 import pronouncing #if unavailable, install first use: pip install pronouncing
 import random
 
-def rhyming_word_generator(end_word):
-    words = []
-    l = pronouncing.rhymes(end_word)
-    for i in range(4):
-        words.append(random.choice(l))
-    return words
+def rhyming_word_generator(rhyming_scheme):
+    for alphabet in rhyming_scheme:
+        #print("current alphabet of scheme: {}".format(alphabet))
+        flag = 0
+        if(bool(rhyme_pattern) == True):
+            for key in rhyme_pattern.keys():
+                #print("key check: {}".format(key))
+                if key == alphabet:
+                    flag = 1
+                    words=rhyme_pattern[alphabet]
+                    print("To match with '{}': {}".format(key, words))
+    
+        if flag == 0:
+            words = []
+            sentence = input("Enter your sentence: ")
+            #print("sentence: {}".format(sentence))
+            sentence_words = sentence.split(" ")
+            #print("sentence words: {}".format(sentence_words))
+            last_word = sentence_words.pop()
+            #print("last words: {}".format(last_word))
+            l = pronouncing.rhymes(last_word)
+            #print(l)
+            for i in range(4):
+                words.append(random.choice(l))
+            rhyme_pattern[alphabet] = words
 
+rhyme_pattern = {} #alphabet: word
 pattern = []
 exit = False
+
 while(exit == False):
     
-    rhyming_scheme = input("Enter rhyming scheme of a stanza: ")
+    rhyming_scheme = input("Enter rhyming scheme of a stanza (space separated): ")
+    if rhyming_scheme == "exit":
+        exit = True
+        continue
     rhyming_pattern = rhyming_scheme.split(" ")
     # This information is useful to recommend in the future
     # if pre-recorded type re-appears, recommend those rhyming words
     #instead of the predecessor words rhyming options
-    
+    """
     for num,i in enumerate(rhyming_pattern):
         #if(flag == 1):
-            
-        sentence = input("Enter your sentence: ")
         
-        #if(sentence == 'exit'):
-        #    exit = True
-        #    break
+        sentence = input("Enter your sentence: ")
         
         print(sentence)
     
@@ -44,10 +64,10 @@ while(exit == False):
     
         last_word = sentence_words.pop()
         print(last_word)
-    
-        answer = rhyming_word_generator(last_word)
+    """
+    rhyming_word_generator(rhyming_pattern)
         #if rhyming_pattern[num+1] in rhyming_pattern[0:num]:
         #    flag = 1
-        print("Termination options for {} : {}".format(last_word,answer))
+        #print("Termination options for {} : {}".format(last_word,answer))
     
     
